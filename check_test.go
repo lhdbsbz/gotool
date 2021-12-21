45eb75fb1758c145e73c9233f7e662786416d8ff
李慧迪 <phone19941961020@163.com>
2021-12-21 18:26:14 +0800

    update

--- check_test.go
+++ check_test.go
@@ -7,7 +7,7 @@ type Temp struct {
 	age  int32
 }
 
-func TestIsEmpty(t *testing.T) {
+func TestIsNotEmpty(t *testing.T) {
 	type args struct {
 		in interface{}
 	}
@@ -21,42 +21,42 @@ func TestIsEmpty(t *testing.T) {
 			args: args{
 				" 1 ",
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "string blank",
 			args: args{
 				" ",
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "string empty",
 			args: args{
 				"",
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "int 1",
 			args: args{
 				1,
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "int 0",
 			args: args{
 				0,
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "struct empty",
 			args: args{
 				Temp{},
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "struct hasValue",
@@ -66,7 +66,7 @@ func TestIsEmpty(t *testing.T) {
 					age:  18,
 				},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "ptr",
@@ -76,49 +76,49 @@ func TestIsEmpty(t *testing.T) {
 					age:  18,
 				},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "ptr empty",
 			args: args{
 				&Temp{},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "nil",
 			args: args{
 				nil,
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "array",
 			args: args{
 				[2]int64{1, 2},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "array empty",
 			args: args{
 				[2]int64{},
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "slice",
 			args: args{
 				[]int64{1, 2},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "slice empty",
 			args: args{
 				[]int64{},
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "map",
@@ -127,19 +127,19 @@ func TestIsEmpty(t *testing.T) {
 					"abc": 1,
 				},
 			},
-			want: false,
+			want: true,
 		},
 		{
 			name: "map empty",
 			args: args{
 				make(map[string]int64),
 			},
-			want: true,
+			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := IsEmpty(tt.args.in); got != tt.want {
+			if got := IsNotEmpty(tt.args.in); got != tt.want {
 				t.Errorf("IsEmpty() = %v, want %v", got, tt.want)
 			}
 		})
